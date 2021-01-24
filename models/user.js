7b224f498d13f58b165ca8a7e29489933a01993e
depgit <surajmishra1234657@gmail.com>
2021-01-24 14:21:30 +0530

    make it better

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    resetToken:String,
+    expireToken:Date,
+    pic:{
+     type:String,
+     default:"https://res.cloudinary.com/depimage/image/upload/v1611431722/naopbmbm2hzvcpmoak4o.jpg"
+    },
+    followers:[{type:ObjectId,ref:"User"}],
+    following:[{type:ObjectId,ref:"User"}]
+})
+
+
+mongoose.model("User",userSchema)
