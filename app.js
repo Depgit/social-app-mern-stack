95bc9aa91e1be693c07b7fd0ba403981df6bc6d1
depgit <surajmishra1234657@gmail.com>
2021-01-24 16:08:41 +0530

    good though

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ mongoose.connect(MONGOURI,{
     useUnifiedTopology: true
 
 })
+
 mongoose.connection.on('connected',()=>{
     console.log("conneted to mongo yeahh")
 })
 mongoose.connection.on('error',(err)=>{
     console.log("err connecting",err)
 })
+
 
 require('./models/user')
 require('./models/post')
